57e0b5ced64d71e39e94bf0c2cc850f010429378
jmardones96 <joaquin.mardones.1996@gmail.com>
2020-05-26 01:06:42 -0400

    Inicio de javascript

---
+++ javascript/funciones.js
@@ -0,0 +1,27 @@
+//Existen 2 tipos de funciones  / Funciones Declarativas / Funciones Expresion
+
+//Funciones Declarativas
+function nomFuncion(){
+    return 3;
+}
+
+
+function saludarEstudiantes(estudiante)
+{
+ console.log(`Hola ${estudiante}`)   
+}
+
+
+function suma (a,b){
+    var resultado = a+b;
+    return resultado;
+}
+
+
+
+
+//Funciones Expresion
+var funcionVariable = function(a , b ){
+    return  a + b;
+}
+
---
+++ javascript/variables.js
@@ -0,0 +1,35 @@
+
+
+
+var nombre = "Joaquin";
+var numero = 2;
+var verdar = true;
+var verdar = false;
+var nulo   = null;
+
+//Estructra de declaracion de variable
+//Declarar
+var edad;
+//Inicializar
+edad = 24;
+
+edad
+
+
+
+//Var array
+var elementos = ["Computador" , "Celular"];
+//LLamado
+elementos[0]
+
+
+//Var Tipo objeto
+var persona = {
+    nombre:"diego",
+    edad:30}
+//LLamado
+    persona.nombre
+
+
+
+
